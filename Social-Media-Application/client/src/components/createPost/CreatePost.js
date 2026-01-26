a1f4eae660a4ce5d258595d177d0ef7a09b79eab
Prajjawal Singh <singhprajjawal87@gmail.com>
2026-01-26 23:21:13 +0530

    fix: restore missing imports and clean up final build errors for Vercel

--- Social-Media-Application/client/src/components/createPost/CreatePost.js
+++ Social-Media-Application/client/src/components/createPost/CreatePost.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import Avatar from "../avatar/Avatar";
 import "./CreatePost.scss";
+import { BsCardImage } from "react-icons/bs";
+import { axiosClient } from "../../utils/axiosClient";
 import { useDispatch, useSelector } from 'react-redux';
 import { getUserProfile } from "../../redux/slices/postsSlice";
 
